cd1bcf6fcd80adcc89f7a579b906cb53be23ad31
ZhangChi <jeorch@foxmail.com>
2019-08-01 19:45:14 +0800

    another way for get disk usage.

--- models/df_test.go
+++ models/df_test.go
@@ -2,8 +2,22 @@ package models
 
 import (
 	"fmt"
+	"syscall"
 	"testing"
 )
+
+func TestGetDiskInfo(t *testing.T) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs("/", &fs)
+	if err != nil {
+		panic(err.Error())
+	}
+	all := fs.Blocks * uint64(fs.Bsize)
+	free := fs.Bfree * uint64(fs.Bsize)
+	fmt.Println("all \t", all)
+	fmt.Println("free \t", free)
+	fmt.Println("usage \t", PercentFormat(float64(all - free)/float64(all) * 100))
+}
 
 func TestGetDiskUsage(t *testing.T) {
 	dfUsage, err := getDiskUsage("/")
